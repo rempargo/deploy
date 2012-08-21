69d68c8cc15b71d4002124e5b4d4fd180337576e
Andrew Kim <andrewtevinkim@gmail.com>
2012-08-21 23:08:38 +0200

    Correct path in railtie

--- lib/paul_rake/railtie.rb
+++ lib/paul_rake/railtie.rb
@@ -4,9 +4,9 @@ require 'rails'
 module MyGem
   class Railtie < Rails::Railtie
   	rake_tasks do
-      load 'lib/tasks/nginx.rake'
-      # This may point to incorrect path. Code below is suggested.
-      # Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+      # load 'lib/tasks/nginx.rake'
+      # this does not work for some reason, code below is suggested
+      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
     end
   end
 end
